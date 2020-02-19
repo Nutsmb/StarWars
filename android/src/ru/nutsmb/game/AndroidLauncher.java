e5361e1c5667889f0360f0fa0bebec1d4882dced
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2020-02-19 23:04:15 +0300

    Start project

---
+++ android/src/ru/nutsmb/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.nutsmb.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.nutsmb.game.StarWars;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarWars(), config);
+	}
+}
