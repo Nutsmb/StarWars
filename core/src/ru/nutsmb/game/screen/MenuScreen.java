df472ece5dc512b6c90dcd1d104d0c7c8a735a62
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2020-02-25 23:01:13 +0300

    Write hard version with vectors

--- core/src/ru/nutsmb/game/screen/MenuScreen.java
+++ core/src/ru/nutsmb/game/screen/MenuScreen.java
@@ -13,6 +13,8 @@ public class MenuScreen extends BaseScreen {
     private Vector2 touch;
     private Vector2 speed;
     private Vector2 position;
+    private Vector2 step;
+    private Vector2 direction;
 
     @Override
     public void show() {
@@ -21,6 +23,8 @@ public class MenuScreen extends BaseScreen {
         touch = new Vector2(img.getWidth(),img.getHeight());
         speed = new Vector2(1,1);
         position = new Vector2();
+        step = new Vector2();
+        direction = new Vector2();
     }
 
     @Override
@@ -29,18 +33,11 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if((position.y+img.getHeight()) < this.touch.y){
-            position.y += speed.y;
+        if(position.dst(this.touch.x, this.touch.y) > step.len()) {
+            position.add(step);
         }
-        else if ((position.y+img.getHeight()) > this.touch.y){
-            position.y -= speed.y;
-        }
-
-        if((position.x+img.getWidth()) < this.touch.x){
-            position.x += speed.x;
-        }
-        else if ((position.x+img.getWidth()) > this.touch.x){
-            position.x -= speed.x;
+        else {
+            position.set(touch);
         }
 
         batch.begin();
@@ -58,6 +55,20 @@ public class MenuScreen extends BaseScreen {
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.x = screenX;
         touch.y = Gdx.graphics.getHeight() - screenY;
+        /* посчитаем направление вектора скорости
+           разделим разность координат по осям на модуль разности
+           т.к. мы работаем в I квадранте, то составляющие direction будут принимать
+           значение  1 при движении слева направо и снизу вверх,и
+           значение -1 при движении справа налево и сверху вниз*/
+        direction.x = (touch.x - position.x) / Math.abs(touch.x - position.x);
+        direction.y = (touch.y - position.y) / Math.abs(touch.y - position.y);
+        // посчитаем тангенс направления движения
+        double tan = (position.y - this.touch.y) / (position.x - this.touch.x);
+        // и проекции скорости на это направление
+        step.x = (float) (speed.len() * (1 / (Math.sqrt(1 + Math.pow(tan, 2)))));
+        step.y = (float) (speed.len() * (1 / (Math.sqrt(1 + Math.pow((1 / tan), 2)))));
+        step.x = step.x * direction.x;
+        step.y = step.y * direction.y;
         return false;
     }
 }
